be05f296a0febb88c5cc8f11cfca0763a30b858b
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-09-11 12:45:39 +0300

    feat: добавляет выбор активной категории

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,26 @@
+import React from "react";
+
 const Categories = () => {
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+    const [activeCategory, setActiveCategory] = React.useState(0);
+    const onClickCategory = (indexCategory) => {
+        setActiveCategory(indexCategory);
+    };
     return (
-      <div className="categories">
-        <ul>
-          <li>Все</li>
-          <li>Мясные</li>
-          <li>Вегетарианская</li>
-          <li>Гриль</li>
-          <li>Острые</li>
-          <li>Закрытые</li>
-        </ul>
-      </div>
+        <div className="categories">
+            <ul>
+                {
+                    categories.map((category, index) => {
+                        return <li
+                            key={category.toString()}
+                            onClick={() => onClickCategory(index)}
+                            className={activeCategory === index ? 'active' : ''}
+                        >{category}</li>
+                    })
+                }
+            </ul>
+        </div>
     );
-  };
+};
 
 export default Categories;
