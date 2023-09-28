384ae19b0b27f34e09f2bb0655da56a7cd72d942
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-09-28 13:17:04 +0300

    optimize: оптимизирует сборку с помощью react-loadable

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,31 @@ import './App.css';
 import './scss/app.scss';
 import Home from './pages/Home';
 import MainLayout from './layouts/MainLayout';
-import React, { Suspense } from 'react';
+import Loadable from 'react-loadable';
 
-const Cart = React.lazy(() => import(/*webpackChunkName: "Cart" */ './pages/Cart'));
-const FullPizza = React.lazy(() => import(/*webpackChunkName: "FullPizza" */ './pages/FullPizza'));
-const NotFound = React.lazy(() => import(/*webpackChunkName: "NotFound" */ './pages/NotFound'));
+const Cart = Loadable({
+  loader: () => import(/*webpackChunkName: "Cart" */ './pages/Cart'),
+  loading: () => <div>Идет загрузка корзины</div>,
+});
+
+const FullPizza = Loadable({
+  loader: () => import(/*webpackChunkName: "FullPizza" */ './pages/FullPizza'),
+  loading: () => <div>Идет загрузка страницы...</div>,
+});
+
+const NotFound = Loadable({
+  loader: () => import(/*webpackChunkName: "NotFound" */ './pages/NotFound'),
+  loading: () => <div>Идет загрузка страницы...</div>,
+});
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="cart" element={
-          <Suspense fallback={<div>Идет загрузка корзины</div>}>
-            <Cart />
-          </Suspense>} />
-        <Route path="pizza/:id" element={
-          <Suspense fallback={<div>Идет загрузка страницы...</div>}>
-            <FullPizza />
-          </Suspense>
-        } />
-        <Route path="*" element={
-          <Suspense fallback={<div>Идет загрузка страницы...</div>}>
-            <NotFound />
-          </Suspense>} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<FullPizza />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
